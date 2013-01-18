9ff9da23ccf82872528bf8a78be9c5e1808139c2
Thomas M. DuBuisson <tommd@galois.com>
2013-01-18 08:26:33 -0500

    Add secure-sockets to the benchmark.

--- Benchmark/Benchmark.hs
+++ Benchmark/Benchmark.hs
@@ -8,6 +8,8 @@ import Control.Concurrent.Async
 import Control.Concurrent
 import Control.Monad
 import Control.Exception (throw)
+import qualified Network.Secure as S
+import OpenSSL
 
 ctxIn :: InContext
 ctxIn  = newInContext (B.replicate 24 0)
@@ -33,12 +35,24 @@ pkg512 = fst $ encode ctxOut x512
 pkg1024 = fst $ encode ctxOut x1024
 pkg2048 = fst $ encode ctxOut x2048
 
-main = do
+main = withOpenSSL $ do
   let secret = B.replicate 32 0
       port1 = 1982
       port2 = 9843
       port3 = 2982
       port4 = 3843
+      port5 = 4843
+      port6 = 5843
+
+  id <- S.newLocalIdentity "localhost" 365
+  S.writeIdentity (S.toPeerIdentity id) >>= B.writeFile "blah.pub"
+  let me = id
+  you <- B.readFile "blah.pub" >>= S.readIdentity
+  server <- S.newServer (Nothing, "4242")
+  aw0   <- async $ S.accept me [you] server
+  c0 <- S.connect me [you] ("localhost", "4242")
+  a0 <- wait aw0
+
 
   aw1 <- async $ accept secret port1 Stream
   threadDelay 10000
@@ -62,6 +76,13 @@ main = do
   c4 <- connectUnsafe secret "127.0.0.1" port4 Stream
   a4 <- wait aw4
 
+  c5 <- connectUnsafe secret "127.0.0.1" port5 Datagram
+  a5 <- connectUnsafe secret "127.0.0.1" port5 Datagram
+  forkIO (forever $ recvUnsafe a5)
+
+  c6 <- connectUnsafe secret "127.0.0.1" port6 Datagram
+  a6 <- connectUnsafe secret "127.0.0.1" port6 Datagram
+
   defaultMain [ bench "encode 16b" $   nf (fst . encode ctxOut) x16
               , bench "encode 32b" $   nf (fst . encode ctxOut) x32
               , bench "encode 64b" $   nf (fst . encode ctxOut) x64
@@ -80,10 +101,17 @@ main = do
               , bench "decode 2048b" $ nf (either throw fst . decode ctxIn) pkg2048
               , bench "send stream 16b" $ send c1 x16
               , bench "send stream 2048b" $ send c1 x2048
+              , bench "send/recv stream 16b" $ (send c2 x16 >> recv a2)
+              , bench "secure-sockets 16b" $ (S.write c0 x16 >> S.read a0 16)
               , bench "send/recv stream 2048b" $ (send c2 x2048 >> recv a2)
-              , bench "sendU stream 16b" $ sendUnsafe c3 x16
-              , bench "sendU stream 2048b" $ sendUnsafe c3 x2048
-              , bench "sendU/recvU stream 2048b" $ (sendUnsafe c4 x2048 >> recvUnsafe a4)
+              , bench "secure-sockets 2048b" $ (S.write c0 x2048 >> S.read a0 2048)
+              -- , bench "sendU stream 16b" $ sendUnsafe c3 x16
+              -- , bench "sendU stream 2048b" $ sendUnsafe c3 x2048
+              -- , bench "sendU/recvU stream 2048b" $ (sendUnsafe c4 x2048 >> recvUnsafe a4)
+
+              -- , bench "sendU dgram 16b" $ sendUnsafe c5 x16
+              -- , bench "sendU dgram 2048b" $ sendUnsafe c5 x2048
+              -- , bench "sendU/recvU dgram 2048b" $ (sendUnsafe c6 x2048 >> recvUnsafe a6)
               -- , bench "send dgram"  $ send c2 x16
               -- , bench "recv stream" $ recv a2
               -- , bench "recv dgram" $ recv a2
